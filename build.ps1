8bcb2f465998ff5e12fa91c85f031d38a123e41e
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2020-10-26 00:25:59 +0100

    Building configs in threads

--- build.ps1
+++ build.ps1
@@ -9,12 +9,30 @@ $vc_edition = "Community";
 $vc_varsall = "$vc_dir\$vc_version\$vc_edition\VC\Auxiliary\Build\vcvarsall.bat"
 $vc_build_project   = "devenv $vcxproj /Project $repo_name /Build"
 
-# Prepare batch command to set Visual C++ env and build the project
-$batch_cmd  = "`"$vc_varsall`" x64";
-$batch_cmd += " && $vc_build_project `"Debug|x86`"";
-$batch_cmd += " && $vc_build_project `"Release|x86`"";
-$batch_cmd += " && $vc_build_project `"Debug|x64`"";
-$batch_cmd += " && $vc_build_project `"Release|x64`"";
+# List of configs to build
+$configs = @(
+    "`"Debug|x86`"",
+    "`"Release|x86`"",
+    "`"Debug|x64`"",
+    "`"Release|x64`""
+)
 
-# Call batch command
-CMD /c $batch_cmd
+# Build configs in threads
+$configs | %{
+    $cmd = "`"$vc_varsall`" x64 && $vc_build_project $_";
+    $script = {
+        param([Parameter()] [string] $arg);
+        CMD /c $arg;
+    }
+    Start-Job -Name $_ -ScriptBlock $script -ArgumentList $cmd;
+}
+"";
+
+# Wait end of all threads
+Get-Job | Wait-Job
+
+# Format and display outputs
+$configs | %{
+    "";"";$_;
+    Get-Job -Name $_ | Receive-Job;
+}
